9115256a7a2b069e9c81ba8c18ccfffceed71a44
hanifisnain8 <hanifisnain8@gmail.com>
2023-10-06 08:55:31 +0700

    1

--- src/belajarbe/Koneksi.java
+++ src/belajarbe/Koneksi.java
@@ -5,21 +5,24 @@ import java.sql.SQLException;
 
 public class Koneksi {
     public static Connection sambung_ke_db(){
-    try{
-        MysqlDataSource m = new MysqlDataSource();
-        m.setUser("root");
-        m.setPassword("pass");
-        m.setDatabaseName("datalapangan");
-        m.setServerName("Localhost");
-        m.setPortNumber(3306);
-        m.setServerTimezone("Asia/Jakarta");
+        try{
+            MysqlDataSource m = new MysqlDataSource();
+            m.setUser("root");
+            m.setPassword("");
+            m.setDatabaseName("datalapangan");
+            m.setServerName("Localhost");
+            m.setPortNumber(3306);
+            m.setServerTimezone("Asia/Jakarta");
         
-        Connection c = m.getConnection();
-        System.out.println("Sukses Terhubung ke database");
-        return c;
-    } catch (SQLException)){
-        System.err.println("Gagal Terhubung ke database");
-        System.err.println("error"+e.getMessage());
+            Connection c = m.getConnection();
+            System.out.println("Sukses Terhubung ke database");
+            return c;
+        } catch (SQLException e){
+            System.err.println("Gagal Terhubung ke database");
+            System.err.println("error"+e.getMessage());
+        }
+        return null;
     }
+
     
 }
